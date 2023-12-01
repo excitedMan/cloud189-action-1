c1db3d3d7491f8a16944502c727917aeabac10f8
EngineerXiao <43105494+excitedMan@users.noreply.github.com>
2023-12-01 13:59:25 +0800

    Update checkin.py
    show url get

--- checkin.py
+++ checkin.py
@@ -144,6 +144,7 @@ def calculate_md5_sign(params):
 def login(username, password):
     url = "https://cloud.189.cn/udb/udb_login.jsp?pageId=1&redirectURL=/main.action"
     r = s.get(url)
+    print(inspect.currentframe().f_lineno, r.text, "\n")
     captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
     lt = re.findall(r'lt = "(.+?)"', r.text)[0]
     returnUrl = re.findall(r"returnUrl = '(.+?)'", r.text)[0]
